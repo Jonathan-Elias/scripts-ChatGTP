f2e8dade58ea2647ca60a7b1091fdb6e17b1e450
Jonathan-Elias <64284629+Jonathan-Elias@users.noreply.github.com>
2023-06-08 13:37:43 -0300

    Create sequncia-script.py
    beta

---
+++ sequncia-script.py
@@ -0,0 +1,9 @@
+import subprocess
+
+# Script em Python
+subprocess.run(['python', 'brutefocer.py'])
+
+# Outro script em Python
+subprocess.run(['python', 'metodos-aceitos.py'])
+
+# ... e assim por diante
