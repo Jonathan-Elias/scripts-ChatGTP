2a2b6fd82ccf98f80d22e5e40d42d4471d6ae31b
Jonathan-Elias <64284629+Jonathan-Elias@users.noreply.github.com>
2023-06-08 12:49:31 -0300

    ftp-anon.py
    beta

---
+++ ftp-anon.py
@@ -0,0 +1,35 @@
+import ftplib
+from tqdm import tqdm  # biblioteca para exibir barra de progresso
+
+# endereço e credenciais do FTP
+host = 'ftp.exemplo.com'
+username = 'anonymous'
+password = 'anonymous'
+
+# nome do arquivo que contém a lista de endereços IP
+filename = 'ips.txt'
+
+# abre o arquivo e lê os endereços IP
+with open(filename) as file:
+    ips = file.readlines()
+    
+# exibe a barra de progresso
+for ip in tqdm(ips, desc='Testando conexão FTP', unit='ip'):
+    ip = ip.strip()  # remove caracteres de quebra de linha
+    
+    try:
+        # cria uma conexão com o servidor FTP
+        ftp = ftplib.FTP(ip)
+        
+        # tenta fazer login com o nome de usuário e senha especificados
+        ftp.login(username, password)
+        
+        # fecha a conexão com o servidor FTP
+        ftp.quit()
+        
+        # exibe uma mensagem de sucesso
+        tqdm.write(f'Credenciais válidas para: {ip}')
+        
+    except ftplib.all_errors:
+        # exibe uma mensagem de erro
+        tqdm.write(f'Credenciais inválidas para: {ip}')
