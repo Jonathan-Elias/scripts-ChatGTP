d3c8576a9ae9648a798409c8ca5fe92b2f29ca52
Jonathan-Elias <64284629+Jonathan-Elias@users.noreply.github.com>
2023-06-08 23:35:36 -0300

    v3 bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,30 +1,49 @@
 import requests
 import json
 import sys
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+session = requests.Session()
 
 def directory_scan(url, wordlist):
     result = []
-    with open(wordlist, 'r') as f:
+    with open(wordlist, 'r', encoding='latin-1') as f:
         paths = f.read().splitlines()
 
     total_paths = len(paths)
     processed_paths = 0
 
-    for path in paths:
-        target_url = f"{url}{path}"
-        response = requests.get(target_url)
-        sys.stdout.write(f"\rTestando: {target_url}")  # Atualiza a linha atual da tela com a URL atual
-        sys.stdout.flush()
+    def scan_path(path):
+        nonlocal result
+
+        target_url = f"{url}{path}"  # Modifica a URL para incluir o caminho do diretório
+        response = session.get(target_url)
 
         if response.status_code == 200:
             result.append(target_url)
-        
+
+        if response.status_code == 200 and os.path.isdir(path):  # Verifica se o caminho é um diretório e executa a atividade principal
+            sub_result = directory_scan(target_url + "/", wordlist)  # Chamada recursiva para as subpastas
+            result.extend(sub_result)  # Adiciona os resultados das subpastas ao resultado principal
+
+            sub_paths = [os.path.join(path, sub_path) for sub_path in os.listdir(path)]
+            sub_result = directory_scan(url, wordlist, sub_paths)  # Chamada recursiva para o brute force das subpastas
+            result.extend(sub_result)  # Adiciona os resultados do brute force das subpastas ao resultado principal
+
+        sys.stdout.write("\033[F")  # Move o cursor para a linha anterior
+        sys.stdout.write("\033[K")  # Limpa a linha atual
+
+        nonlocal processed_paths
         processed_paths += 1
         progress = processed_paths / total_paths * 100
-        sys.stdout.write(f"\rProgresso: [{progress:.2f}%]")  # Atualiza a linha atual da tela com o progresso atual
+        sys.stdout.write(f"\rProgresso: [{progress:.2f}%]")
         sys.stdout.flush()
 
-    sys.stdout.write("\n")  # Move para uma nova linha após o teste ser concluído
+    with ThreadPoolExecutor() as executor:
+        executor.map(scan_path, paths)
+
+    sys.stdout.write("\n")
     return result
 
 def save_to_json(result, filename):
@@ -39,7 +58,7 @@ def main():
     result = directory_scan(url, wordlist)
     save_to_json(result, f"{output_file}.json")
 
-    print(f"O resultado do teste foi salvo em {output_file}.json")
+    print(f"\nO resultado do teste foi salvo em {output_file}.json")
     print("Resultados com código de retorno 200:")
     for res in result:
         print(res)
