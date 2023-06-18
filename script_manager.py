c9e81657c79f6f0460411cb81947380a0c458576
Jonathan-Elias <64284629+Jonathan-Elias@users.noreply.github.com>
2023-06-18 13:48:54 -0300

    Script Manager!
    Este script permite executar scripts de forma interativa.
    Selecione um número correspondente ao script desejado.

---
+++ script_manager.py
@@ -0,0 +1,60 @@
+import subprocess
+
+scripts = {
+    1: {
+        'name': 'Brute-Force',
+        'command': ['python', 'bruteforce.py']
+    },
+    2: {
+        'name': 'Métodos-aceitos',
+        'command': ['python', 'metodos-aceitos.py']
+    },
+    3: {
+        'name': 'Compartilhamento-SMBv1',
+        'command': ['python', 'smbv1.py']
+    },
+    4: {
+        'name': 'Compartilhamento-SMBv1-Rede',
+        'command': ['python', 'smbv1rede.py']
+    },
+    # Adicione mais scripts conforme necessário
+}
+
+def execute_script(script):
+    subprocess.run(script['command'])
+
+def print_scripts():
+    print("Selecione o script a ser executado:")
+    for num, script in scripts.items():
+        print(f"{num}. {script['name']}")
+
+def print_banner():
+    banner = """
+======================================================
+Bem-vindo ao Script Manager!
+Este script permite executar scripts de forma interativa.
+Selecione um número correspondente ao script desejado.
+Digite 0 para sair do programa.
+======================================================
+"""
+    print(banner)
+
+def main():
+    print_banner()
+    while True:
+        print_scripts()
+        selected = int(input("Número do script: "))
+        if selected == 0:
+            break
+        script = scripts.get(selected)
+        if script:
+            execute_script(script)
+        else:
+            print("Script inválido.")
+        
+        response = input("Deseja executar outro script? (s/n): ")
+        if response.lower() != 's':
+            break
+
+if __name__ == "__main__":
+    main()
